eb3ba79f6be28996387d658ce56c9eb6a555ef79
ariz <ariz@DESKTOP-HPGEHVM>
2024-10-07 18:13:38 +0200

    firstname

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,7 +39,7 @@ class RegisteredUserController extends Controller
 
         $user = User::create([
             'name' => $request->name,
-            'fistname' => $request->firstName,
+            'firstname' => $request->firstName,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'created_at' => now(),
